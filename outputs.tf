e30f560c899e89eb2ce30e3684805f707e2c9b9d
jiantony <jibinantony@gmail.com>
2024-01-22 00:04:10 +1100

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "domain_name" {
-  description = "Public DNS name of the EC2 instance."
-  value       = aws_instance.web.public_dns
-}
-
-output "application_url" {
-  description = "URL of the example application."
-  value       = "${aws_instance.web.public_dns}/index.php"
-}
